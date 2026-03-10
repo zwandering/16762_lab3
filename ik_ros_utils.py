e58a6daa7bfda289bd6b6d1eb66ab061b4d63375
Haokun Zhu <haokunz@andrew.cmu.edu>
2026-03-10 19:47:39 -0400

    update

--- ik_ros_utils.py
+++ ik_ros_utils.py
@@ -7,7 +7,7 @@ from tf2_geometry_msgs import TransformStamped
 
 # lift up to table height, wrist yaw in line with base, wrist pitch slightly down, gripper open
 READY_POSE_P1 = {
-    'joint_lift': 0.8,
+    'joint_lift': 0.5,
     'joint_wrist_yaw': 1.5,
     'joint_wrist_pitch': -0.1,
     'gripper_aperture': 0.5
@@ -15,7 +15,7 @@ READY_POSE_P1 = {
 
 READY_POSE_P2 = {
     'joint_lift': 0.8 ,
-    'joint_arm_l0': 0.0,
+    'joint_arm': 0.0,
     'joint_wrist_yaw': 0.0,
     'joint_wrist_pitch': -0.1,
     'gripper_aperture': 0.5,
