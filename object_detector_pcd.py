7aea64b289f04b41a3257c2079aa4e444d46f84e
Haokun Zhu <haokunz@andrew.cmu.edu>
2026-03-10 21:32:32 -0400

    update

--- object_detector_pcd.py
+++ object_detector_pcd.py
@@ -121,7 +121,7 @@ class YOLOEObjectDetector(Node):
             detections = None
         else:
             # Run YOLO-E object detection on the color frame
-            results = self.model(self.latest_color, verbose=False, thres=0.25)
+            results = self.model(self.latest_color, verbose=False, conf=0.2)
             # Parse the detection results
             detections = detection_utils.parse_results(results)
 
